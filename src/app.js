adb2efbb8fa7b762c2dc8baa50834967a5c751b8
Yadav Adhikari <yadav.adhikari@sgic.cooperators.ca>
2019-04-21 10:30:57 -0600

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const instantgrid = require('./utils/instantgrid')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //console.log(__dirname)
 //console.log(path.join(__dirname, '../public'))
@@ -100,6 +101,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port' + port)
 })
