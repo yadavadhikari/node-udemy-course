432713c25f415bc521b5ec9177942e0d7125ce23
Yadav Adhikari <yadav.adhikari@sgic.cooperators.ca>
2019-04-22 14:39:29 -0600

    Add new dataset

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,20 +1,34 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (lat, lng, callback) => {
+  const url =
+    "https://api.darksky.net/forecast/914adc62bc1ad667f63a59c3c4a88014/" +
+    lat +
+    "," +
+    lng +
+    "?units=si";
 
-    const url = 'https://api.darksky.net/forecast/914adc62bc1ad667f63a59c3c4a88014/' + lat + ',' + lng + '?units=si'
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Can not connect to forecast service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find the location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          " It is currently " +
+          body.currently.temperature +
+          " degrees out. There is " +
+          body.currently.precipProbability +
+          "% chance of rain. \n" +
+          "Daily high is " +
+          body.daily.data[0].temperatureMax +
+          " and the low is " +
+          body.daily.data[0].temperatureMin
+      );
+    }
+  });
+};
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Can not connect to forecast service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find the location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is '
-                + body.currently.precipProbability + '% chance of rain.')
-        }
-    })
-
-}
-
-module.exports = forecast
+module.exports = forecast;
